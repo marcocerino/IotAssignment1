e9f0571f06730d08a3ce8033e5314b79df0912af
Marco Cerino <cerin.marco97@gmail.com>
2020-03-23 10:28:25 +0100

    fixxed comments

--- valueGenerator/mqtt_client.py
+++ valueGenerator/mqtt_client.py
@@ -1,36 +1,36 @@
-import paho.mqtt.client as paho  		    #mqtt library
+import paho.mqtt.client as paho  		    				
 import os
 import json
 import time
 from datetime import datetime
 from sensors import Sensors
 
-ACCESS_TOKEN='67v7kSViFKVYDccBawvd'                 #Token of your device
-broker="demo.thingsboard.io"   			    #host name
-port=1883 					    #data listening port
+ACCESS_TOKEN='67v7kSViFKVYDccBawvd'                						#Token of the device
+broker="demo.thingsboard.io"   			    							#host name
+port=1883 					    										#data listening port
 
-def on_publish(client,userdata,result):             #create function for callback
+def on_publish(client,userdata,result):             					#create function for callback
     print("data published to thingsboard \n")
     pass
     
-client1= paho.Client("control1")                    #create client object
-client1.on_publish = on_publish                     #assign function to callback
-client1.username_pw_set(ACCESS_TOKEN)               #access token from thingsboard device
-client1.connect(broker,port,keepalive=60)           #establish connection
+client1= paho.Client(client_id="control1")                    			#create client object
+client1.on_publish = on_publish                     					#assign function to callback
+client1.username_pw_set(ACCESS_TOKEN)              						#access token from thingsboard device
+client1.connect(broker,port,keepalive=60)           					#establish connection
 
-sensors = Sensors()                                 #create sensor obj
+sensors = Sensors()                                						#create sensor obj
 
-while True:
-	values = sensors.getValue()
-	payload="{"
+while True:																#get the values
+	values = sensors.getValue()	
+	payload="{"															#create message payload
   	payload+="\"Temperature\":"+ values[0] +"," ; 
    	payload+="\"Humidity\":"+ values[1] +"," ; 
    	payload+="\"WindDirection\":"+ values[2] +"," ; 
 	payload+="\"WindIntensity\":"+ values[3] +"," ; 
 	payload+="\"RainHight\":"+ values[4]; 
-	payload+="}"
-	ret= client1.publish("v1/devices/me/telemetry",payload) #topic-v1/devices/me/telemetry
+	payload+="}"				
+	ret= client1.publish("v1/devices/me/telemetry",payload) 			#topic-v1/devices/me/telemetry
 	print("Please check LATEST TELEMETRY field of your device")
 	print(payload);
-	time.sleep(5)
+	time.sleep(5)														#sleep for 5 seconds
    
