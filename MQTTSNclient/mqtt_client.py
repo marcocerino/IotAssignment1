c18ab5ed741f2a0cca2adc6eda7ebc818d3f517c
Marco Cerino <cerin.marco97@gmail.com>
2020-03-28 16:27:47 +0100

    completed the bridge script

---
+++ MQTTSNclient/mqtt_client.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as paho  		    				
+import os
+import json
+import time
+from datetime import datetime
+
+class MQTT_client:
+	def __init__(self,ACCESS_TOKEN='67v7kSViFKVYDccBawvd',broker="demo.thingsboard.io",port=1883):
+		self.client= paho.Client(client_id="control1")                    			#create client object
+		self.client.on_publish = self.on_publish                     					#assign function to callback
+		self.client.username_pw_set(ACCESS_TOKEN)              						#access token from thingsboard device
+		self.client.connect(broker,port,keepalive=60)           					#establish connection
+		print("connected to the broker")
+
+
+	def on_publish(client,userdata,result):
+		print("data published to thingsboard \n")
+    	
+	def publish(self,payload): 
+		print("payload:")
+		print(payload )
+		ret= self.client.publish("v1/devices/me/telemetry",payload) 			#topic-v1/devices/me/telemetry
+		print("Please check LATEST TELEMETRY field of your device")
+   	
+   	def disconnect(self):
+   		self.client.disconnect()
+   		print("disconnected from MQTT broker")
--- MQTTSNclient/myclient.py
+++ MQTTSNclient/myclient.py
@@ -1,34 +1,34 @@
 import MQTTSNclient as mqttsn
 import Queue as q
 import time
+import mqtt_client as mqtt
 
-msg_queue = q.Queue()
-class MyCallback:
-	def messageArrived(self, topicName, payload, qos, retained, msgid):
-	 m= "d-p-Arrived" +" topic  " +str(TopicId)+ "message " +\
-	 str(payload) +"  qos= " +str(qos) +" ret= " +str(retained)\
-	 +"  msgid= " + str(msgid)
-	 print("got the message ",payload)
-	 msg_queue.put(payload)
+ACCESS_TOKEN='67v7kSViFKVYDccBawvd'                						#Token of the device
+broker="demo.thingsboard.io"   			    							#host name
+port=1883 					    										#data listening port
+
+print("connecting to the online MQTT broker")
+mclient = mqtt.MQTT_client(ACCESS_TOKEN,broker,port)
+
+class MyCallback:														
+	def messageArrived(self, topicName, payload, qos, retained, msgid):		#callback function on message arrival
+	 print "default publishArrived"
+	 ret= mclient.publish(payload) 			
 	 return True
 
-def empty_queue():
-   while not msg_queue.empty():
-      m=msg_queue.get()
-      print("Received message  ",m)
 
 aclient = mqttsn.Client("linh", port=1885)
-aclient.registerCallback(mqttsn.Callback())
+aclient.registerCallback(MyCallback())
 aclient.connect()
-
-
 aclient.subscribe("v1/devices/me/telemetry")
+
+
 try:
 	while(1):
 		time.sleep(2)
-		empty_queue()
 except KeyboardInterrupt:
 	print("Closing the client")
 	aclient.disconnect()
+	mclient.disconnect()
 
 
