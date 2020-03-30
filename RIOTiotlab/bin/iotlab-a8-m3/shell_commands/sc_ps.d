c4622cd4f538b4306de07b79ee85ac8ed46c5f4a
Marco Cerino <cerin.marco97@gmail.com>
2020-03-30 13:11:52 +0200

    .

--- RIOTiotlab/bin/iotlab-a8-m3/shell_commands/sc_ps.d
+++
@@ -1,8 +0,0 @@
-/home/cerino/Uni/IOT/IotAssignment1/RIOTiotlab/bin/iotlab-a8-m3/shell_commands/sc_ps.o: \
- /home/cerino/RIOT/sys/shell/commands/sc_ps.c \
- /home/cerino/Uni/IOT/IotAssignment1/RIOTiotlab/bin/iotlab-a8-m3/riotbuild/riotbuild.h \
- /home/cerino/RIOT/sys/include/ps.h
-
-/home/cerino/Uni/IOT/IotAssignment1/RIOTiotlab/bin/iotlab-a8-m3/riotbuild/riotbuild.h:
-
-/home/cerino/RIOT/sys/include/ps.h:
